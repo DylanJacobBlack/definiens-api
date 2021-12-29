7e75c0d9683cc789cb0980d35a1eeb933a2690ce
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-29 19:26:41 +0800

    add route and controller for creating lessons

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -1,7 +1,22 @@
 # app/controllers/api/v1/lessons_controller.rb
 class Api::V1::LessonsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
+
   def index
     @lessons = Lesson.all
     render json: @lessons #Just for testing
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      flash[:success] = "Lesson successfully created"
+    else
+      flash[:error] = "Something went wrong"
+    end
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :text, :language_id, :user_id)
   end
 end
