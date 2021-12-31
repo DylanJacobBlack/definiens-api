af43e2ab7b0ab779d1ae274668aa757c21e67f6a
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-31 20:06:23 +0800

    Add json pages using jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :lessons, only: %i[index create]
+      resources :lessons, only: %i[index show create]
     end
   end
 end
