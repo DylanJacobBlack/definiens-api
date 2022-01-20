5b3fab91e244d32eab577903185c5b3d5859243d
Dylan Black <dylan.jacob.black@gmail.com>
2022-01-20 16:52:04 +0800

    Trying out alternative credentials

--- app/controllers/definition_controller.rb
+++ app/controllers/definition_controller.rb
@@ -2,10 +2,12 @@ class DefinitionController < ApplicationController
   require "google/cloud/translate/v2"
 
   def lookup
-    translate = Google::Cloud::Translate::V2.new version: :v2
+    translate = Google::Cloud::Translate::V2.new(
+    project_id: "glossy-motif-334504",
+    credentials: "./glossy-motif-334504-ee91c048ab7c.json")
 
     translation = translate.translate params[:text], to: "la"
 
-    render json:  { translation: translation.text } #=> "Salve mundi!"
+    render json:  {translation: translation.text} #=> "Salve mundi!"
   end
 end
