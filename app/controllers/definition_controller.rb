8451cf7b679baabf39a8c5cae51aa8af8800defa
Dylan Black <dylan.jacob.black@gmail.com>
2022-01-20 17:04:58 +0800

    Change credentials in definition_controller, fourth time's charm...

--- app/controllers/definition_controller.rb
+++ app/controllers/definition_controller.rb
@@ -4,7 +4,7 @@ class DefinitionController < ApplicationController
   def lookup
     translate = Google::Cloud::Translate::V2.new(
     project_id: "glossy-motif-334504",
-    credentials: "credentials.json")
+    credentials: "google-credentials.json")
 
     translation = translate.translate params[:text], to: "la"
 
