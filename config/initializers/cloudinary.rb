b9fb2cb2a724319f96091ae01b4c22f6ddf68869
Dylan Black <dylan.jacob.black@gmail.com>
2022-01-24 20:27:01 +0800

    Delete redundant cloudinary.rb

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = 'dqlx6iqqt'
-    config.api_key = '535683285638263'
-    config.api_secret = 's3O0o816AAbOVphgLEwF7jiijaA'
-    config.secure = true
-    config.cdn_subdomain = true
-  end
