2716446ea1c0dcbeb1d1b20e32e832837b42f4da
Dylan Black <dylan.jacob.black@gmail.com>
2022-02-23 06:35:58 -0600

    Add validation for words

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,7 @@ class Word < ApplicationRecord
   belongs_to :lesson
   belongs_to :user
   belongs_to :language
+  validates :name, presence: true, uniqueness: true
+  validates :definition, presence: true
+  validates :known, numericality: { only_integer: true }
 end
