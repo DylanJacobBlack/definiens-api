3a61c765c1bc384ec08dedd036e50d6453140a68
Dylan Black <dylan.jacob.black@gmail.com>
2022-02-25 06:12:19 -0600

    Correct words controller

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -11,6 +11,6 @@ class Api::V1::WordsController < Api::V1::BaseController
 
   def create
     @word = Word.create(translation: params[:word][:translation], name: params[:word][:name], language_id: params[:word][:language_id], user_id: params[:word][:user_id], home_language: "en" )
-    @lesson.nil? ? :not_found : :created
+    @word.nil? ? :not_found : :created
   end
 end
