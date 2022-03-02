5469dac47d916ead28f6ce688e28ca8bd20d5e08
Dylan Black <dylan.jacob.black@gmail.com>
2022-03-02 10:24:46 -0600

    Support adding new words using button on the frontend

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -11,8 +11,12 @@ class Api::V1::WordsController < Api::V1::BaseController
   end
 
   def create
-    puts params[:word][:translation]
-    @word = Word.create(translation: params[:word][:translation], name: params[:word][:name], language_id: params[:word][:language_id], home_language: "en", user: @current_user, known: 0)
+    language = Language.where(name: params[:word][:language]).first
+    @word = Word.create(translation: params[:word][:translation], name: params[:word][:name], language: language, home_language: "en", user: @current_user, known: 0)
     @word.nil? ? :not_found : :created
+  end
+
+  def words_params
+    params.require(:word).permit(:translation, :name, :language)
   end
 end
